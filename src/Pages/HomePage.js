86cc5cd2be4d1a23781ea589762c99516f090343
Tomohiro Sugiyama <tomo.soul.15@gmail.com>
2021-02-20 00:50:24 +0900

    first commit

---
+++ src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+//Before I start to 'rfce + tab key'
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>HomePage</h1>
+    </div>
+  );
+}
+
+export default HomePage;
