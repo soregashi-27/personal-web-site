8f1f8df0ef0846cf59ae0834f3fd044dd431cea8
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-06 23:54:10 +0900

    イメージセクションとプロフィールを追加

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -5,7 +5,8 @@ import ImageSection from './ImageSection';
 function AboutPage() {
   return (
     <div className="AboutPage">
-      <h1>About Page</h1>
+      {/* <h1>About Page</h1> */}
+      {/* FIXME:最後に削除 */}
       <Tittle title={'About Me'} span={'About Me'} />
       <ImageSection />
     </div>
--- src/Pages/ImageSection.js
+++ src/Pages/ImageSection.js
@@ -13,8 +13,26 @@ function ImageSection() {
           <span>杉山友啓</span>
           です。
         </h4>
-        <p>自己紹介を追加します。只今編集中です。</p>
-        <div className="about-details"></div>
+        <p className="about-text">自己紹介を追加します。只今編集中です。</p>
+        <div className="about-details">
+          <div className="left-section">
+            <p>Full Name</p>
+            <p>Age</p>
+            <p>Nationalty</p>
+            <p>Languages</p>
+            <p>Address</p>
+            <p>Countries</p>
+          </div>
+          <div className="right-section">
+            <p>: Tomohiro Sugiyama</p>
+            <p>: 27</p>
+            <p>: Japan</p>
+            <p>: Japanese, English</p>
+            <p>: Hakata-ku, Fukuoka city</p>
+            <p>: Japan</p>
+          </div>
+        </div>
+        <button className="btn">Download My Cv</button>
       </div>
     </div>
   );
