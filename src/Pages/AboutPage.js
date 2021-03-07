2670e3458dadc284760801b52d3782584180804d
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-07 17:31:05 +0900

    スキルセクションの更新

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -11,13 +11,14 @@ function AboutPage() {
       <Tittle title={'About Me'} span={'About Me'} />
       <ImageSection />
       <Tittle title={'My Skills'} span={'My Skills'} />
-      <div className="skills-container">
+      <div className="SkillsContainer">
+        {/* FIXME:リファクタリング時にclass名を変える↑(_about.scss) */}
         <SkillsSection skill={'Javacript'} progress={'60%'} width={'60%'} />
         <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
         <SkillsSection skill={'React.js'} progress={'70%'} width={'70%'} />
         <SkillsSection skill={'Node.js'} progress={'80%'} width={'80%'} />
         <SkillsSection skill={'Python'} progress={'90%'} width={'90%'} />
-        <SkillsSection skill={'Lua'} progress={'40%'} width={'40%'} />
+        <SkillsSection skill={'Vue.js'} progress={'40%'} width={'40%'} />
         <SkillsSection skill={'Kotlin'} progress={'80%'} width={'80%'} />
         <SkillsSection skill={'Web Design'} progress={'25%'} width={'25%'} />
         <SkillsSection skill={'UI/UX Design'} progress={'76%'} width={'76%'} />
