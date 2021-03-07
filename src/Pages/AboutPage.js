bc40853ef98eecde6daf5c398965974376c2e7f2
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-07 16:40:31 +0900

    スキルの追加

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tittle from '../Components/Tittle';
-import ImageSection from './ImageSection';
+import ImageSection from '../Components/ImageSection';
+import SkillsSection from '../Components/SkillsSection';
 
 function AboutPage() {
   return (
@@ -9,6 +10,18 @@ function AboutPage() {
       {/* FIXME:最後に削除 */}
       <Tittle title={'About Me'} span={'About Me'} />
       <ImageSection />
+      <Tittle title={'My Skills'} span={'My Skills'} />
+      <div className="skills-container">
+        <SkillsSection skill={'Javacript'} progress={'60%'} width={'60%'} />
+        <SkillsSection skill={'Typescript'} progress={'30%'} width={'30%'} />
+        <SkillsSection skill={'React.js'} progress={'70%'} width={'70%'} />
+        <SkillsSection skill={'Node.js'} progress={'80%'} width={'80%'} />
+        <SkillsSection skill={'Python'} progress={'90%'} width={'90%'} />
+        <SkillsSection skill={'Lua'} progress={'40%'} width={'40%'} />
+        <SkillsSection skill={'Kotlin'} progress={'80%'} width={'80%'} />
+        <SkillsSection skill={'Web Design'} progress={'25%'} width={'25%'} />
+        <SkillsSection skill={'UI/UX Design'} progress={'76%'} width={'76%'} />
+      </div>
     </div>
   );
 }
