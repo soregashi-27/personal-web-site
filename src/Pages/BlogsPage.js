a5817bf489b830b4227d346336a1de0b397aeb21
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-27 17:48:45 +0900

    ブログページを追加

--- src/Pages/BlogsPage.js
+++ src/Pages/BlogsPage.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import allBlogs from '../Components/allBlogs';
+import Tittle from '../Components/Tittle';
 
 function BlogsPage() {
   return (
     <div>
-      <h1>Blogs</h1>
+      <div className="blog-tittle">
+        <Tittle title={'Blogs'} span={'Blogs'} />
+      </div>
+      <div className="BlogsPage">
+        {allBlogs.map((blog) => {
+          return (
+            <div className="blog" key={blog.id}>
+              <div className="blog-content">
+                <img src={blog.image} alt="" />
+                <a href={blog.link} className="blog-link">
+                  {blog.title}
+                </a>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
