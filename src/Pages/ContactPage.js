299faf6bb8179e7a741695c8e4a9c8ce876cd8b8
soregashi-27 <tomo.soul.15@gmail.com>
2021-03-28 17:55:51 +0900

    コンタクトページの追加

--- src/Pages/ContactPage.js
+++ src/Pages/ContactPage.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ContactItem from '../Components/ContactItem';
+import Tittle from '../Components/Tittle';
+import email from '../img/emailme.svg';
+import location from '../img/location.svg';
 
 function ContactPage() {
   return (
-    <div className="ContactPage">
-      <div className="map-sect"></div>
-      <div className="contact-sect">
-        <ContactItem />
-        <ContactItem />
-        <ContactItem />
+    <div>
+      <div className="title">
+        <Tittle title={'Contact'} span={'Contact'} />
+      </div>
+      <div className="ContactPage">
+        <div className="map-sect"></div>
+        {/* FIXME:map-sectは削除 */}
+        <div className="contact-sect">
+          {/* FIXME: change name at contact-section */}
+          <ContactItem
+            icon={email}
+            title={'e-mail'}
+            text1={'test@sample.com'}
+            text2={'textAdd'}
+          />
+          <ContactItem
+            icon={location}
+            title={'Location'}
+            text1={'Based on JP'}
+            text2={'textAdd'}
+          />
+          {/* contact-formを設置してもいいかも。（google form or original） */}
+        </div>
       </div>
     </div>
   );
